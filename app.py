7a5a684e36f7e5c590e74acfd04118d167522af4
Kite <e48f@M-C02X73SVJHD2.nordstrom.net>
2020-02-20 09:09:49 -0800

    add signup initial controller

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, request
+from controllers.auth import signup_controller
 
 app = Flask(__name__)
 
 @app.route('/signup', methods=['POST'])
 def signup():
   payload = request.get_json()
-  return signup(payload)
+  response = signup_controller(payload)
+  return response
 
 @app.errorhandler(404)
 def not_found(error):
--- controllers/auth.py
+++ controllers/auth.py
@@ -1,3 +1,9 @@
+from flask import jsonify
 
-def signup(payload):
+def signup_controller(payload={}):
+    if 'email' not in payload or 'password' not in payload or 'passwordMatch' not in payload:
+        return jsonify('Missing information'), 400
+    if payload['password'] != payload['passwordMatch']:
+        return jsonify('Passwords do not match'), 400
+    return jsonify('Looking good'), 200
     
